6fa7b01bca36a4fb128cd3c1962a02134907677e
MinhTuan352 <kirigayatuna@gmail.com>
2025-05-20 09:58:55 +0700

    Update module-info.java để chạy được Painter.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,9 @@
 module HUST_Book_Store {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
+    exports hust.soict.program.javafx;
+    opens hust.soict.program.javafx to javafx.fxml;
     exports frontend;
 }
