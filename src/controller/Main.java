3bb19cb7348da8506dcb09b91bc5ab19ee90215b
MinhTuan352 <kirigayatuna@gmail.com>
2025-05-28 22:29:02 +0700

    Up lại Main.java

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -6,52 +6,33 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.manager.AppServiceManager;
+import model.manager.user.UserManager;
+import model.product.Stationery;
+import model.product.Toy;
+import model.product.book.Book;
+import model.user.User;
+
+
+
 
 public class Main extends Application {
 
-    private static AppServiceManager appServiceManager = new AppServiceManager();
-
+    public static AppServiceManager appServiceManager = new AppServiceManager();
+    public static UserManager userManager = appServiceManager.getUserManager();
+    public static User currentUser ;
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            // Tạo FXMLLoader để tải HomePage.fxml
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/customer/HomePage.fxml"));
-            
-            // Sử dụng setControllerFactory để tiêm AppServiceManager vào controller
-            loader.setControllerFactory(controllerClass -> {
-                try {
-                    // Tạo instance của controller
-                    Object controller = controllerClass.getDeclaredConstructor().newInstance();
-                    // Gọi setAppServiceManager nếu controller có phương thức này
-                    try {
-                        controllerClass.getMethod("setAppServiceManager", AppServiceManager.class)
-                            .invoke(controller, appServiceManager);
-                    } catch (NoSuchMethodException e) {
-                        System.out.println("Controller " + controllerClass.getName() + " không có phương thức setAppServiceManager. Bỏ qua.");
-                    }
-                    return controller;
-                } catch (Exception e) {
-                    throw new RuntimeException("Không thể khởi tạo controller: " + controllerClass.getName(), e);
+                launch(args);
+            }
+        
+            @Override
+            public void start(Stage primaryStage) {
+                try{
+                    Parent root = FXMLLoader.load(this.getClass().getResource("/view/Login.fxml"));
+                    Scene scene = new Scene(root);
+                    primaryStage.setScene(scene);
+                    primaryStage.show();
+                }catch (Exception e){
+                    System.out.println(e);
                 }
-            });
-
-            // Tải FXML
-            Parent root = loader.load();
-            Scene scene = new Scene(root);
-            primaryStage.setTitle("HustBookStore");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (Exception e) {
-            System.err.println("Lỗi khởi động ứng dụng: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-    public static AppServiceManager getAppServiceManager() {
-        return appServiceManager;
-    }
+            }
 }
