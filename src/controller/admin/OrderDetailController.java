4fdae5643bc903aee0dc2ae1c34136e7d1a34dbc
Chivu171 <tranchivu171@gmail.com>
2025-05-30 07:26:03 +0700

    Update OrderDetailController.java

--- src/controller/admin/OrderDetailController.java
+++ src/controller/admin/OrderDetailController.java
@@ -6,11 +6,15 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label; // <<--- Đảm bảo là dòng này
+import controller.Main;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 import model.order.Order;
+import model.product.Product;
 import model.user.cart.CartItem;
+import model.manager.AppServiceManager;
 import model.manager.order.OrderManager;
 
 
@@ -29,7 +33,7 @@ public class OrderDetailController {
     private Label orderIdLabel;
 
     @FXML
-    private ListView<CartItem> orderItemsListView;
+    private ListView<String> orderItemsListView;
 
     @FXML
     private Label orderStatusLabel;
@@ -43,6 +47,8 @@ public class OrderDetailController {
     @FXML
     private Label totalAmountLabel;
 
+    private AppServiceManager appServiceManager = Main.appServiceManager;
+
     // Phương thức để nhận dữ liệu từ OrderManagement
     public void setOrder(Order order) {
         orderIdLabel.setText(order.getOrderId());
@@ -53,7 +59,24 @@ public class OrderDetailController {
         paymentMethodLabel.setText(order.getPaymentMethod());
         customerUsernameLabel.setText(order.getCustomerUsername());
         
-        orderItemsListView.setItems(FXCollections.observableArrayList(order.getOrderItems()));
+        if (order != null && order.getOrderItems() != null && appServiceManager != null && appServiceManager.getProductManager() != null) {
+            ObservableList<String> productDisplayList = FXCollections.observableArrayList();
+            for (CartItem item : order.getOrderItems()) {
+                Product product = appServiceManager.getProductManager().getProductById(item.getProductId());
+                if (product != null) {
+                    // Chỉ lấy title:
+                    productDisplayList.add(product.getTitle());
+        
+                    // Hoặc tạo chuỗi hiển thị phức tạp hơn:
+                    // productDisplayList.add(String.format("%s - Số lượng: %d", product.getTitle(), item.getQuantity()));
+                } else {
+                    productDisplayList.add("Sản phẩm không tìm thấy (ID: " + item.getProductId() + ")");
+                }
+            }
+            orderItemsListView.setItems(productDisplayList);
+        } else {
+            orderItemsListView.setItems(FXCollections.emptyObservableList()); // Xử lý trường hợp null
+        }
             
             
 
